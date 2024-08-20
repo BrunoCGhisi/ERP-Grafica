39001e9f40d71d0c3c730b309c29c34b80a4141a
MaJoApol <mariajoanaceron@gmail.com>
2024-08-20 17:29:04 -0300

    comecei_controller

---
+++ controllers/clientesController.py
@@ -0,0 +1,65 @@
+from flask import request
+from database.db import db
+from models.usuarios import Usuarios
+
+def usuariosController():
+
+    if request.method == 'POST':
+        try:
+            data = request.get_json() # converte em python
+            usuarios = Usuarios(data['nome'], data['email'], data['senha'], data['isAdm'])
+            db.session.add(usuarios)
+            db.session.commit()
+            return 'Usuarios adicionados com sucesso!', 200
+        except Exception as e:
+            return f'Não foi possível inserir. Erro {str(e)}', 405
+        
+
+    elif request.method == 'GET':
+        try:
+            data = Usuarios.query.all()
+            newData = {'usuarios': [autor.to_dict() for autor in data]} #pegando cada obj autor, e tranformando num dicionario
+            return newData, 200
+
+        except Exception as e:
+            return f'Não foi possível buscar. Erro {str(e)}', 405
+        
+
+    elif request.method == 'PATCH':
+            try:
+                id = request.args.to_dict().get('id')
+                usuario = Usuarios.query.get(id)
+                data = request.get_json() #pega todos os dados
+
+                
+                if usuario is None:
+                    return{'error': 'Usuário não encontrado'}, 405
+                
+                usuario.nome = data.get('nome', usuario.nome)
+                usuario.email = data.get('email', usuario.email)   
+                usuario.senha = data.get('senha', usuario.senha)   
+                usuario.isAdm = data.get('isAdm', usuario.isAdm)   
+
+                db.session.commit()
+                return "Usuário atualizado com sucesso", 202
+
+            except Exception as e:
+                return f"Não foi possível atualizar o autor. Erro:{str(e)}", 405
+            
+
+    elif request.method == 'DELETE':
+        try:
+            id = request.args.to_dict().get('id') #pega o id dos dados que o data trouxe do front
+            usuario = Usuarios.query.get(id) # vai procurar usuarios NO BANCO com esse id
+
+            if usuario is None:
+                return{'error': 'Usuário não encontrado'}, 405
+            
+            db.session.delete(usuario)
+            db.session.commit()
+            return "Usuário deletado com sucesso", 202
+
+        except Exception as e:
+            return f"Não foi possível apagar o usuário. Erro:{str(e)}", 405
+        
+
