04c38661606bbe1c3193c2e1c055e8d0570f02ad
Bruno Costa Ghisi <brunocostaghisi@gmail.com>
2024-11-02 15:54:16 -0300

    get bancos controller & router

--- routes/bancosRoutes.py
+++ routes/bancosRoutes.py
@@ -1,4 +1,6 @@
 from controllers.bancosController import bancosController
+from controllers.bancosController import getBancos
 
 def bancosRoutes(app):
         app.route('/banco', methods=['GET','POST','PUT','DELETE'])(bancosController)
+        app.route('/banco/itens', methods=['GET'])(getBancos) 
