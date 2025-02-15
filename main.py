55913674391812277efe4b5c5ae3b8dfffd22429
Bruno Costa Ghisi <brunocostaghisi@gmail.com>
2025-02-15 10:43:29 -0300

    comitei

--- main.py
+++ main.py
@@ -23,5 +23,5 @@ class MyServer(): #classe que inicializa e guarda o funcionamento do flask
 
 if __name__ == '__main__' :
     app = MyServer() #instanciando
-    app.run() #inicializando
+    app.run() 
             
